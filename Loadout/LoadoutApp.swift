55e39cd8a491c1f2321ac09d9fb3e6914515173b
danyobosayo <sungsu.kim04@gmail.com>
2026-05-09 20:24:33 -0500

    Initial Commit

---
+++ Loadout/LoadoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadoutApp.swift
+//  Loadout
+//
+//  Created by Daniel Sungsu Kim on 2026/5/9.
+//
+
+import SwiftUI
+
+@main
+struct LoadoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
